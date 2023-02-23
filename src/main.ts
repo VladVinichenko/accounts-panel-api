5856630102899d42775d2128dacd835a417aa310
VladVinichenko <vinnyxxxl925@gmail.com>
2023-02-23 04:14:56 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 8080
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,   {cors: {
+    origin: process.env.APP_URL,
+  }},);
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
